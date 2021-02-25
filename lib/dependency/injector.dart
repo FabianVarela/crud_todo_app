3965fc51d47626d63f655df311121f74db4464e2
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-02-24 22:50:03 -0500

    Modify inject and main initializing firebase

--- lib/dependency/injector.dart
+++ lib/dependency/injector.dart
@@ -4,7 +4,7 @@ import 'package:get_it/get_it.dart';
 
 GetIt getIt = GetIt.instance;
 
-Future<void> setupInject() async {
+void setupInject() {
   _loadPackages();
   _loadDatabase();
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:crud_todo_app/dependency/injector.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 Future<void> main() async {
-  await setupInject();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  setupInject();
+  await Firebase.initializeApp();
+
   runApp(ProviderScope(child: TodoListApp()));
 }
 
