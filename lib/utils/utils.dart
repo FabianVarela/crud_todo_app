010097c69684b2ed1d56bfab44e32005d8d06956
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-02-26 23:44:01 -0500

    Create extension method to validate emoji

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_emoji/flutter_emoji.dart';
 import 'package:intl/intl.dart';
 
 extension DateTimeUtils on DateTime {
@@ -7,4 +8,14 @@ extension DateTimeUtils on DateTime {
 
 extension IntUtils on int {
   DateTime get millisecondsToDate => DateTime.fromMillisecondsSinceEpoch(this);
+}
+
+extension StringUtils on String {
+  bool get verifyEmoji {
+    for (final s in EmojiParser().unemojify(this).split(' ')) {
+      return s.startsWith(':') || s.endsWith(':');
+    }
+
+    return false;
+  }
 }
