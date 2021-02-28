b39179e45dc8f341b2b5df6f78c063f6e5d7110c
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-02-28 15:22:56 -0500

    Update category model adding todoSize

--- lib/model/category.model.dart
+++ lib/model/category.model.dart
@@ -2,25 +2,30 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_emoji/flutter_emoji.dart';
 
 class Category {
-  Category({this.id, this.name, this.emoji});
+  Category({this.id, this.name, this.emoji, this.todoSize});
 
   final String id;
   final String name;
   final Emoji emoji;
+  final int todoSize;
 
   Category.fromSnapshot(DocumentSnapshot snapshot)
       : id = snapshot.id,
         name = snapshot.data()['name'],
-        emoji = EmojiParser().get(snapshot.data()['emoji']);
+        emoji = EmojiParser().get(snapshot.data()['emoji']),
+        todoSize = (snapshot.data()['todoSize'] ?? 0);
 
   Map<String, dynamic> toJson() => <String, dynamic>{
         'name': name,
         'emoji': emoji.full,
+        'todoSize': todoSize ?? 0
       };
 
-  Category copyWith({String id, String name, Emoji emoji}) => Category(
+  Category copyWith({String id, String name, Emoji emoji, int todoSize}) =>
+      Category(
         id: id ?? this.id,
         name: name ?? this.name,
         emoji: emoji ?? this.emoji,
+        todoSize: todoSize ?? this.todoSize,
       );
 }
