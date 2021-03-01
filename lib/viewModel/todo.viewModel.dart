c3c4006afa40cc97c3d1f13f1069580af0b7a6e5
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-02-28 19:04:25 -0500

    Update todo viewModel add validations

--- lib/viewModel/todo.viewModel.dart
+++ lib/viewModel/todo.viewModel.dart
@@ -1,6 +1,8 @@
 import 'package:crud_todo_app/database/todo.database.dart';
 import 'package:crud_todo_app/dependency/injector.dart';
 import 'package:crud_todo_app/model/todo.model.dart';
+import 'package:crud_todo_app/model/validation_text.model.dart';
+import 'package:crud_todo_app/utils/validations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final todoDataProvider = StreamProvider.family<List<Todo>, String>(
@@ -10,34 +12,38 @@ final todoDataProvider = StreamProvider.family<List<Todo>, String>(
 final isAddedTodoProvider = StateProvider.autoDispose((_) => false);
 
 final idTodoProvider = StateProvider.autoDispose((_) => '');
-final subjectTodoProvider = StateProvider.autoDispose((_) => '');
+final subjectTodoProvider = StateProvider.autoDispose((_) => ValidationText());
 final dateTodoProvider = StateProvider.autoDispose((_) => DateTime.now());
 
 final validationTodoProvider = StateProvider.autoDispose((ref) {
-  final subject = ref.watch(subjectTodoProvider).state;
+  final subject = ref.watch(subjectTodoProvider).state.text;
   final date = ref.watch(dateTodoProvider).state;
 
-  return subject.isNotEmpty && !date.difference(DateTime.now()).isNegative;
+  return (subject ?? '').isNotEmpty &&
+      !date.difference(DateTime.now()).isNegative;
 });
 
 final todoViewModelProvider = Provider((_) => TodoViewModel());
 
-class TodoViewModel {
+class TodoViewModel with Validations {
   final _database = getIt<ITodoDatabase>();
 
-  void saveTodo(String todoId, String todoSubject, bool todoCompleted,
-      String categoryId) async {
-    final todo = Todo(
-      id: todoId,
-      subject: todoSubject,
-      isCompleted: todoCompleted,
-      finalDate: DateTime.now(),
-      categoryId: categoryId,
-    );
-    await _database.saveTodo(todo, categoryId);
-  }
+  ValidationText onChangeSubject(String value) => validateEmpty(value);
 
-  void removeTodo(String todoId) async => await _database.deleteTodo(todoId);
+  void saveTodo(String id, String subject, bool isOk, String catId) async =>
+      await _database.saveTodo(
+        Todo(
+          id: id,
+          subject: subject,
+          isCompleted: isOk,
+          finalDate: DateTime.now(),
+          categoryId: catId,
+        ),
+        catId,
+      );
+
+  void removeTodo(String todoId, String catId) async =>
+      await _database.deleteTodo(todoId, catId);
 
   void checkTodo(Todo todo, String categoryId) async => await _database
       .saveTodo(todo.copyWith(isCompleted: !todo.isCompleted), categoryId);
