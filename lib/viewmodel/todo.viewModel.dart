3a3b70294672dc26d7cdf96265b4e4aa5663abbe
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-04-15 13:09:20 -0500

    Update TODO's viewModel

--- lib/viewmodel/todo.viewModel.dart
+++ lib/viewmodel/todo.viewModel.dart
@@ -1,13 +1,13 @@
-import 'package:crud_todo_app/database/todo.database.dart';
-import 'package:crud_todo_app/dependency/injector.dart';
+import 'package:crud_todo_app/common/validations.dart';
+import 'package:crud_todo_app/provider_dependency.dart';
+import 'package:crud_todo_app/repository/todo.repository.dart';
 import 'package:crud_todo_app/model/todo.model.dart';
 import 'package:crud_todo_app/model/validation_text.model.dart';
-import 'package:crud_todo_app/utils/utils.dart';
-import 'package:crud_todo_app/utils/validations.dart';
+import 'package:crud_todo_app/common/utils.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final todoDataProvider = StreamProvider.family<List<Todo>, String>(
-  (ref, catId) => getIt<ITodoDatabase>().getTodosByCategory(catId),
+  (ref, catId) => ref.read(todoRepositoryProvider).getTodosByCategory(catId),
 );
 
 final isAddedTodoProvider = StateProvider.autoDispose((_) => false);
@@ -23,26 +23,20 @@ final validationTodoProvider = StateProvider.autoDispose((ref) {
   return (subject ?? '').isNotEmpty && !date.isDurationNegative;
 });
 
-final todoViewModelProvider = Provider((_) => TodoViewModel());
-
 class TodoViewModel with Validations {
-  final _database = getIt<ITodoDatabase>();
+  late final ITodoRepository _repository;
+
+  TodoViewModel(this._repository);
 
   ValidationText onChangeSubject(String value) => validateEmpty(value);
 
-  void saveTodo(String id, String subject, bool isOk, DateTime finalDate,
-          String catId) async =>
-      await _database.saveTodo(Todo(
-        id: id,
-        subject: subject,
-        isCompleted: isOk,
-        finalDate: finalDate,
-        categoryId: catId,
-      ));
+  void saveTodo(String id, String subject, DateTime date, String catId) async =>
+      await _repository.saveTodo(
+          Todo(id: id, subject: subject, finalDate: date, categoryId: catId));
 
   void removeTodo(String todoId, String catId) async =>
-      await _database.deleteTodo(todoId, catId);
+      await _repository.deleteTodo(todoId, catId);
 
   void checkTodo(Todo todo, bool isChecked) async =>
-      await _database.saveTodo(todo.copyWith(isCompleted: isChecked));
+      await _repository.saveTodo(todo.copyWith(isCompleted: isChecked));
 }
