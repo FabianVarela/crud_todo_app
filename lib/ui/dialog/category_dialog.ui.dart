2fc5e02a93c48e92e2fee0454db93a3c38223504
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-04-29 13:13:21 -0500

    Update category dialog UI

--- lib/ui/dialog/category_dialog.ui.dart
+++ lib/ui/dialog/category_dialog.ui.dart
@@ -1,6 +1,7 @@
 import 'package:crud_todo_app/common/utils.dart';
 import 'package:crud_todo_app/provider_dependency.dart';
-import 'package:crud_todo_app/viewmodel/category.viewModel.dart';
+import 'package:crud_todo_app/viewmodel/category/category_provider.dart';
+import 'package:crud_todo_app/viewmodel/category/category_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -8,19 +9,13 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 class CategoryFormDialog extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    final name = useProvider(nameCatProvider);
-    final emoji = useProvider(emojiCatProvider);
+    final categoryViewModel = useProvider(categoryViewModelProvider);
 
-    final nameText = useTextEditingController(text: name.state.text ?? '');
-    final emojiText = useTextEditingController(text: emoji.state.text ?? '');
-
-    return ProviderListener<StateController<bool>>(
-      provider: isAddedCategoryProvider,
-      onChange: (_, addedState) {
-        if (addedState.state) {
-          WidgetsBinding.instance!.addPostFrameCallback(
-            (_) => Navigator.pop(context),
-          );
+    return ProviderListener(
+      provider: categoryViewModelProvider,
+      onChange: (_, CategoryState state) {
+        if (state == CategoryState.success()) {
+          Navigator.pop(context);
         }
       },
       child: Column(
@@ -30,9 +25,8 @@ class CategoryFormDialog extends HookWidget {
           GestureDetector(
             onTap: () => Navigator.of(context).pop(),
             child: Icon(Icons.close, color: Colors.white),
-          ).paddingVertical(5),
+          ).paddingSymmetric(v: 5),
           Container(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
             decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.circular(12),
@@ -41,76 +35,84 @@ class CategoryFormDialog extends HookWidget {
               children: <Widget>[
                 Text(
                   'Add category',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-                ),
-                const SizedBox(height: 15),
-                _nameTextField(context, nameText),
-                const SizedBox(height: 5),
-                _emojiTextField(context, emojiText),
-                const SizedBox(height: 25),
-                _categoryButton(name.state.text ?? '', emoji.state.text ?? ''),
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ).paddingOnly(b: 15),
+                _Name().paddingOnly(b: 5),
+                _Emoji().paddingOnly(b: 25),
+                if (categoryViewModel != CategoryState.loading()) _Submit(),
+                if (categoryViewModel == CategoryState.loading())
+                  CircularProgressIndicator()
               ],
-            ),
+            ).paddingSymmetric(h: 16, v: 10),
           ),
         ],
       ),
     );
   }
+}
 
-  Widget _nameTextField(BuildContext ctx, TextEditingController controller) {
-    return Consumer(
-      builder: (_, watch, __) => TextField(
-        controller: controller,
-        textInputAction: TextInputAction.next,
-        decoration: InputDecoration(
-          hintText: 'Name',
-          errorText: watch(nameCatProvider).state.message,
-        ),
-        onChanged: (value) => watch(nameCatProvider).state =
-            ctx.read(categoryViewModelProvider).onChangeName(value),
+class _Name extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final nameText = useProvider(nameCatProvider);
+    final textController = useTextEditingController(
+      text: nameText.state.text ?? '',
+    );
+
+    return TextField(
+      controller: textController,
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        hintText: 'Name',
+        errorText: nameText.state.message,
       ),
+      onChanged: (value) => nameText.state =
+          context.read(categoryViewModelProvider.notifier).onChangeName(value),
     );
   }
+}
 
-  Widget _emojiTextField(BuildContext ctx, TextEditingController controller) {
-    return Consumer(
-      builder: (_, watch, __) => TextField(
-        controller: controller,
-        textInputAction: TextInputAction.done,
-        decoration: InputDecoration(
-          hintText: 'Emoji',
-          errorText: watch(emojiCatProvider).state.message,
-        ),
-        onChanged: (value) => watch(emojiCatProvider).state =
-            ctx.read(categoryViewModelProvider).onChangeEmoji(value),
+class _Emoji extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final emoji = useProvider(emojiCatProvider);
+    final textController = useTextEditingController(
+      text: emoji.state.text ?? '',
+    );
+
+    return TextField(
+      controller: textController,
+      textInputAction: TextInputAction.done,
+      decoration: InputDecoration(
+        hintText: 'Emoji',
+        errorText: emoji.state.message,
       ),
+      onChanged: (value) => emoji.state =
+          context.read(categoryViewModelProvider.notifier).onChangeEmoji(value),
     );
   }
+}
 
-  Widget _categoryButton(String name, String emoji) {
-    return Consumer(builder: (_, watch, __) {
-      final categoryViewModel = watch(categoryViewModelProvider);
+class _Submit extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final categoryViewModel = context.read(categoryViewModelProvider.notifier);
+    final isValid = useProvider(validationCategoryProvider).state;
 
-      final isValid = watch(validationCategoryProvider).state;
-      final isAdded = watch(isAddedCategoryProvider);
-
-      return ElevatedButton(
-        style: ElevatedButton.styleFrom(primary: Color(0xFF4A78FA)),
-        onPressed: isValid
-            ? () {
-                categoryViewModel.saveCategory(name, emoji);
-                isAdded.state = true;
-              }
-            : null,
-        child: Container(
-          width: double.infinity,
-          alignment: Alignment.center,
-          child: Text(
-            'Create',
-            style: TextStyle(fontSize: 16, color: Colors.white),
-          ),
-        ).paddingVertical(12),
-      );
-    });
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(primary: Color(0xFF4A78FA)),
+      onPressed: isValid ? categoryViewModel.saveCategory : null,
+      child: Container(
+        width: double.infinity,
+        alignment: Alignment.center,
+        child: Text(
+          'Create',
+          style: TextStyle(fontSize: 16, color: Colors.white),
+        ),
+      ).paddingSymmetric(v: 12),
+    );
   }
 }
