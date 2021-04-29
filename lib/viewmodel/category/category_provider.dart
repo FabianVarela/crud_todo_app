a2d3ef28765b48af840bb78b1089692863585bf6
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-04-29 13:11:53 -0500

    Move providers from category view model to category provider

---
+++ lib/viewmodel/category/category_provider.dart
@@ -0,0 +1,22 @@
+import 'package:crud_todo_app/common/utils.dart';
+import 'package:crud_todo_app/model/category.model.dart';
+import 'package:crud_todo_app/model/validation_text.model.dart';
+import 'package:crud_todo_app/provider_dependency.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final categoryDataProvider = StreamProvider<List<Category>>(
+  (ref) => ref.watch(todoRepositoryProvider).getCategories(),
+);
+
+final isAddedCategoryProvider = StateProvider.autoDispose((_) => false);
+
+final nameCatProvider = StateProvider.autoDispose((_) => ValidationText());
+final emojiCatProvider = StateProvider.autoDispose((_) => ValidationText());
+
+final validationCategoryProvider = StateProvider.autoDispose((ref) {
+  final name = ref.watch(nameCatProvider).state.text;
+  final emoji = ref.watch(emojiCatProvider).state.text;
+
+  final isValidEmoji = ((emoji ?? '').isNotEmpty && (emoji ?? '').verifyEmoji);
+  return (name ?? '').isNotEmpty && isValidEmoji;
+});
