f7d15b265be5426c639000eb50b1d6ebf97c23ab
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-04-29 12:57:48 -0500

    Update extension methods

--- lib/common/utils.dart
+++ lib/common/utils.dart
@@ -36,18 +36,15 @@ extension WidgetUtils on Widget {
         child: this,
       );
 
-  Widget paddingHorizontal(double padding) => Padding(
-        padding: EdgeInsets.symmetric(horizontal: padding),
+  Widget paddingSymmetric({double h = 0.0, double v = 0.0}) => Padding(
+        padding: EdgeInsets.symmetric(horizontal: h, vertical: v),
         child: this,
       );
 
-  Widget paddingVertical(double padding) => Padding(
-        padding: EdgeInsets.symmetric(vertical: padding),
-        child: this,
-      );
-
-  Widget paddingSymmetric(double hori, double vert) => Padding(
-        padding: EdgeInsets.symmetric(horizontal: hori, vertical: vert),
-        child: this,
-      );
+  Widget paddingOnly({double l = 0, double t = 0, double r = 0, double b = 0}) {
+    return Padding(
+      padding: EdgeInsets.only(left: l, top: t, bottom: b, right: r),
+      child: this,
+    );
+  }
 }
