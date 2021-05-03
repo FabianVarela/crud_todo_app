cb903de0d26f4c1f183a98d2e503a9345ca2372f
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-05-02 21:09:47 -0500

    Update todo list view

---
+++ lib/ui/todo_list.view.dart
@@ -0,0 +1,158 @@
+import 'package:crud_todo_app/model/category.model.dart';
+import 'package:crud_todo_app/model/todo.model.dart';
+import 'package:crud_todo_app/provider_dependency.dart';
+import 'package:crud_todo_app/ui/add_todo.view.dart';
+import 'package:crud_todo_app/common/utils.dart';
+import 'package:crud_todo_app/ui/widgets/todo_item.dart';
+import 'package:crud_todo_app/viewmodel/todo/todo_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class TodoListView extends HookWidget {
+  const TodoListView({Key? key, required this.category}) : super(key: key);
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    final todoStream = useProvider(todoDataProvider(category.id!));
+    final categoryVm = context.read(categoryViewModelProvider.notifier);
+
+    return Scaffold(
+      backgroundColor: Color(0xFF4A78FA),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+          onPressed: () => Navigator.pop(context),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () {
+              categoryVm.deleteCategory(category.id!);
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  height: 60,
+                  width: 60,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Text(
+                    category.emoji.code,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      category.name,
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white,
+                      ),
+                    ).paddingOnly(b: 5),
+                    Text(
+                      '${category.todoSize} Tasks',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w300,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ).paddingOnly(l: 35),
+          ),
+          Expanded(
+            flex: 2,
+            child: todoStream.when(
+              data: (todos) {
+                return Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
+                  child: todos.isNotEmpty
+                      ? _todoList(context, todos).paddingSymmetric(h: 24, v: 20)
+                      : _emptyMessage(),
+                );
+              },
+              loading: () => const Center(child: CircularProgressIndicator()),
+              error: (e, s) => Container(
+                child: Center(
+                  child: Text(e.toString(), style: TextStyle(fontSize: 20)),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xFF4A78FA),
+        onPressed: () => _goToTodo(context),
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget _todoList(BuildContext context, List<Todo> todos) {
+    final viewModel = context.read(todoViewModelProvider.notifier);
+
+    return ListView.builder(
+      itemCount: todos.length,
+      itemBuilder: (_, pos) => TodoItem(
+        item: todos[pos],
+        category: category,
+        onEdit: () => _goToTodo(context, todo: todos[pos]),
+        onRemove: () =>
+            viewModel.removeTodo(todos[pos].id, todos[pos].categoryId),
+        onCheck: (value) => viewModel.checkTodo(todos[pos], value),
+      ),
+    );
+  }
+
+  Widget _emptyMessage() {
+    return Center(
+      child: Text(
+        'Empty data, add a task',
+        style: TextStyle(fontSize: 25),
+      ),
+    );
+  }
+
+  void _goToTodo(BuildContext context, {Todo? todo}) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) => AddTodoView(category: category, todo: todo),
+      ),
+    );
+  }
+}
