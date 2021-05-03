dc0a52ae15f368242a2db3c0dc269825239bc025
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-05-02 21:03:52 -0500

    Change todoViewModelProvider as StateNotifierProvider

--- lib/provider_dependency.dart
+++ lib/provider_dependency.dart
@@ -2,7 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:crud_todo_app/repository/todo.repository.dart';
 import 'package:crud_todo_app/viewmodel/category/category_view_model.dart';
 import 'package:crud_todo_app/viewmodel/category/category_state.dart';
-import 'package:crud_todo_app/viewmodel/todo.viewModel.dart';
+import 'package:crud_todo_app/viewmodel/todo/todo_view_model.dart';
+import 'package:crud_todo_app/viewmodel/todo/todo_state.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 /// Firestore
@@ -21,6 +22,6 @@ final categoryViewModelProvider =
     StateNotifierProvider<CategoryViewModel, CategoryState>(
         (ref) => CategoryViewModel(ref.read));
 
-final todoViewModelProvider = Provider(
-  (ref) => TodoViewModel(ref.watch(todoRepositoryProvider)),
+final todoViewModelProvider = StateNotifierProvider<TodoViewModel, TodoState>(
+  (ref) => TodoViewModel(ref.read),
 );
