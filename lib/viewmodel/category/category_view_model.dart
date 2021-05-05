d35f13a093a75eddeb9f1429ebae44e242e5e6a8
Frank Moreno <frankmoreno1993@gmail.com>
2021-05-04 20:45:37 -0500

    Refactor with ICategoryViewModel

--- lib/viewmodel/category/category_view_model.dart
+++ lib/viewmodel/category/category_view_model.dart
@@ -7,10 +7,16 @@ import 'package:crud_todo_app/viewmodel/category/category_state.dart';
 import 'package:flutter_emoji/flutter_emoji.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class CategoryViewModel extends StateNotifier<CategoryState> with Validations {
-  late final Reader _read;
+import '../../model/validation_text.model.dart';
+import '../../repository/todo.repository.dart';
 
-  CategoryViewModel(this._read) : super(const CategoryState.initial());
+abstract class ICategoryViewModel extends StateNotifier<CategoryState>
+    with Validations {
+  ICategoryViewModel() : super(const CategoryState.initial());
+
+  ValidationText get nameCat;
+  ValidationText get emojiCat;
+  ITodoRepository get todoRepo;
 
   ValidationText onChangeName(String value) => validateEmpty(value);
 
@@ -20,10 +26,10 @@ class CategoryViewModel extends StateNotifier<CategoryState> with Validations {
     try {
       state = CategoryState.loading();
 
-      final name = _read(nameCatProvider).state;
-      final emoji = _read(emojiCatProvider).state;
+      final name = nameCat;
+      final emoji = emojiCat;
 
-      await _read(todoRepositoryProvider).saveCategory(Category(
+      await todoRepo.saveCategory(Category(
         name: name.text!,
         emoji: EmojiParser().getEmoji(emoji.text!),
       ));
@@ -37,10 +43,25 @@ class CategoryViewModel extends StateNotifier<CategoryState> with Validations {
   void removeCategory(String id) async {
     try {
       state = CategoryState.loading();
-      await _read(todoRepositoryProvider).deleteCategory(id);
+      await todoRepo.deleteCategory(id);
       state = CategoryState.success(CategoryAction.remove);
     } on Exception catch (e) {
       state = CategoryState.error(e.toString());
     }
   }
+}
+
+class CategoryViewModel extends ICategoryViewModel {
+  CategoryViewModel(this._read);
+
+  late final Reader _read;
+
+  @override
+  ValidationText get nameCat => _read(nameCatProvider).state;
+
+  @override
+  ValidationText get emojiCat => _read(emojiCatProvider).state;
+
+  @override
+  ITodoRepository get todoRepo => _read(todoRepositoryProvider);
 }
