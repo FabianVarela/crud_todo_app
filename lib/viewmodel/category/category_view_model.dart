049822c975b5878df03bf220ee7940621466ca96
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-05-07 19:08:55 -0500

    Refactor variables in category_view_model.dart

--- lib/viewmodel/category/category_view_model.dart
+++ lib/viewmodel/category/category_view_model.dart
@@ -2,21 +2,21 @@ import 'package:crud_todo_app/common/validations.dart';
 import 'package:crud_todo_app/provider_dependency.dart';
 import 'package:crud_todo_app/model/category.model.dart';
 import 'package:crud_todo_app/model/validation_text.model.dart';
+import 'package:crud_todo_app/repository/todo.repository.dart';
 import 'package:crud_todo_app/viewmodel/category/category_provider.dart';
 import 'package:crud_todo_app/viewmodel/category/category_state.dart';
 import 'package:flutter_emoji/flutter_emoji.dart';
 import 'package:crud_todo_app/common/common.dart';
-
-import '../../model/validation_text.model.dart';
-import '../../repository/todo.repository.dart';
 
 abstract class ICategoryViewModel extends StateNotifier<CategoryState>
     with Validations {
   ICategoryViewModel() : super(const CategoryState.initial());
 
   ValidationText get nameCat;
+
   ValidationText get emojiCat;
-  ITodoRepository get todoRepo;
+
+  ITodoRepository get todoRepository;
 
   ValidationText onChangeName(String value) => validateEmpty(value);
 
@@ -29,7 +29,7 @@ abstract class ICategoryViewModel extends StateNotifier<CategoryState>
       final name = nameCat;
       final emoji = emojiCat;
 
-      await todoRepo.saveCategory(Category(
+      await todoRepository.saveCategory(Category(
         name: name.text!,
         emoji: EmojiParser().getEmoji(emoji.text!),
       ));
@@ -43,7 +43,7 @@ abstract class ICategoryViewModel extends StateNotifier<CategoryState>
   void removeCategory(String id) async {
     try {
       state = CategoryState.loading();
-      await todoRepo.deleteCategory(id);
+      await todoRepository.deleteCategory(id);
       state = CategoryState.success(CategoryAction.remove);
     } on Exception catch (e) {
       state = CategoryState.error(e.toString());
@@ -63,5 +63,5 @@ class CategoryViewModel extends ICategoryViewModel {
   ValidationText get emojiCat => _read(emojiCatProvider).state;
 
   @override
-  ITodoRepository get todoRepo => _read(todoRepositoryProvider);
+  ITodoRepository get todoRepository => _read(todoRepositoryProvider);
 }
