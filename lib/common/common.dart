16689b2b18ab77c165aba1215c7515e128c0cb0e
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-05-07 23:53:12 -0500

    move widget_reference.dart file

--- lib/common/common.dart
+++ lib/common/common.dart
@@ -2,4 +2,4 @@ export 'utils.dart';
 export 'validations.dart';
 
 //export 'package:hooks_riverpod/hooks_riverpod.dart';
-export '../temporal/widget_reference.dart';
+export 'temporal/widget_reference.dart';
