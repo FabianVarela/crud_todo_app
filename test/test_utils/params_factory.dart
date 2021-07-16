30dc6e373dc4494f3d7fd8be766d49b6eca875a3
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-07-15 22:52:02 -0500

    Create params_factory.dart to set stub entities

---
+++ test/test_utils/params_factory.dart
@@ -0,0 +1,26 @@
+import 'package:crud_todo_app/model/category_model.dart';
+import 'package:crud_todo_app/model/todo_model.dart';
+import 'package:flutter_emoji/flutter_emoji.dart';
+
+const categoryId = 'ABC123';
+const categoryName = 'Test Category';
+final categoryEmoji = EmojiParser().get('😀');
+
+Category get category => Category(
+      id: categoryId,
+      name: categoryName,
+      emoji: categoryEmoji,
+      todoSize: 0,
+    );
+
+const todoId = 'TODO-456';
+const todoSubject = 'Test TODO';
+final todoFinalDate = DateTime.now().add(const Duration(days: 10));
+
+Todo get todo => Todo(
+      categoryId: categoryId,
+      id: todoId,
+      finalDate: todoFinalDate,
+      subject: todoSubject,
+      isCompleted: false,
+    );
