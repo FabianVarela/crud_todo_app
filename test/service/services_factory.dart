42c57a5cd7dc3decb5fd0ddda9c1d5da731da6b0
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-07-21 14:09:48 -0500

    Disable mockito use

--- test/service/services_factory.dart
+++ test/service/services_factory.dart
@@ -1,9 +1,12 @@
+/** THIS IS AN IMPLEMENTATION WITH MOCKITO, BUT ACTUALLY DOESN'T USE **/
+
+/*
 import 'package:crud_todo_app/service/category_service.dart';
 import 'package:crud_todo_app/service/todo_service.dart';
 import 'package:mockito/annotations.dart';
 
 @GenerateMocks(
-  [TodoService, CategoryService],
+  [TodoService],
   customMocks: [
     MockSpec<DateTime>(
       as: #ArgelDateTimeMock,
@@ -12,3 +15,4 @@ import 'package:mockito/annotations.dart';
   ],
 )
 void main() {}
+*/
