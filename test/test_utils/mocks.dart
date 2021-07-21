2aea8fb2e2e2eb5503df4b710d11132ad19e5f1a
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-07-21 14:10:17 -0500

    Set all mocks in mocks.dart file

--- test/test_utils/mocks.dart
+++ test/test_utils/mocks.dart
@@ -1,3 +1,7 @@
+import 'package:crud_todo_app/model/category_model.dart';
+import 'package:crud_todo_app/model/todo_model.dart';
+import 'package:crud_todo_app/service/category_service.dart';
+import 'package:crud_todo_app/service/todo_service.dart';
 import 'package:crud_todo_app/viewmodel/category/category_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:mocktail/mocktail.dart';
@@ -7,6 +11,18 @@ import 'package:mocktail/mocktail.dart';
 class MockNavigator extends Mock implements NavigatorObserver {}
 
 class MyRouteFake extends Fake implements Route<dynamic> {}
+
+/// Services
+
+class MockCategoryService extends Mock implements CategoryService {}
+
+class MockTodoService extends Mock implements TodoService {}
+
+/// Models
+
+class MyCategoryFake extends Fake implements Category {}
+
+class MyTodoFake extends Fake implements Todo {}
 
 /// ViewModels
 
