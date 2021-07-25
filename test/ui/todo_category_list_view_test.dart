cddfea8f5a3584a7eddf720561ba7764a894bb56
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-07-25 18:50:08 -0500

    Update test methods in todo_category_list_view_test.dart

--- test/ui/todo_category_list_view_test.dart
+++ test/ui/todo_category_list_view_test.dart
@@ -1,10 +1,10 @@
 import 'package:crud_todo_app/model/category_model.dart';
 import 'package:crud_todo_app/provider_dependency.dart';
 import 'package:crud_todo_app/repository/category_repository.dart';
+import 'package:crud_todo_app/ui/dialog/category_dialog.dart';
 import 'package:crud_todo_app/ui/todo_category_list_view.dart';
 import 'package:crud_todo_app/ui/todo_list_view.dart';
 import 'package:crud_todo_app/ui/widgets/category_item.dart';
-import 'package:crud_todo_app/viewmodel/category/category_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -14,47 +14,35 @@ import '../test_utils/mocks.dart';
 import '../test_utils/params_factory.dart';
 
 void main() {
-  late MockNavigator mockNavigator;
-
-  late MockCategoryService mockCategoryService;
-  late ICategoryRepository categoryRepository;
-
-  Widget _categoryListWithData() {
-    return Scaffold(
-      body: Consumer(
-        builder: (_, ref, __) {
-          final categories = ref.watch(categoryDataProvider);
-
-          if (categories.data == null) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          final list = categories.data!.value;
-          return list.isNotEmpty
-              ? GridView.count(crossAxisCount: 2, children: <Widget>[
-                  for (final item in list)
-                    CategoryItem(item: item, onClick: () {}),
-                ])
-              : const Offstage();
-        },
-      ),
-    );
-  }
-
-  setUpAll(() {
-    mockCategoryService = MockCategoryService();
-    categoryRepository = CategoryRepository(mockCategoryService);
-    registerFallbackValue(MyCategoryFake());
-
-    mockNavigator = MockNavigator();
-    registerFallbackValue(MyRouteFake());
-  });
-
   group('$TodoCategoryListView UI screen', () {
-    testWidgets('Show $TodoCategoryListView screen', (tester) async {
-      await tester.pumpWidget(const ProviderScope(
-        child: MaterialApp(home: TodoCategoryListView()),
+    late MockCategoryService mockCategoryService;
+    late ICategoryRepository categoryRepository;
+
+    late MockNavigator mockNavigator;
+
+    setUpAll(() {
+      mockCategoryService = MockCategoryService();
+      categoryRepository = CategoryRepository(mockCategoryService);
+      registerFallbackValue(MyCategoryFake());
+
+      mockNavigator = MockNavigator();
+      registerFallbackValue(MyRouteFake());
+    });
+
+    Future<void> _pumpMainScreen(WidgetTester tester, Widget child) async {
+      await tester.pumpWidget(ProviderScope(
+        overrides: [
+          categoryRepositoryProvider.overrideWithValue(categoryRepository),
+        ],
+        child: MaterialApp(
+          home: child,
+          navigatorObservers: [mockNavigator],
+        ),
       ));
+    }
+
+    testWidgets('Show $TodoCategoryListView screen', (tester) async {
+      await _pumpMainScreen(tester, const TodoCategoryListView());
 
       expect(find.byIcon(Icons.menu_rounded), findsOneWidget);
       expect(find.text('Lists'), findsOneWidget);
@@ -62,52 +50,45 @@ void main() {
     });
 
     testWidgets(
+        'Show $Dialog section in $TodoCategoryListView screen '
+        'when set tap in $FloatingActionButton', (tester) async {
+      await _pumpMainScreen(tester, const TodoCategoryListView());
+
+      final finderFloatingButton = find.byType(FloatingActionButton);
+
+      await tester.tap(finderFloatingButton);
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+
+      expect(find.byType(Dialog), findsOneWidget);
+      expect(find.byType(CategoryFormDialog), findsOneWidget);
+
+      expect(find.byIcon(Icons.close), findsOneWidget);
+      expect(find.text('Add category'), findsOneWidget);
+      expect(find.byType(NameCategory), findsOneWidget);
+      expect(find.byType(EmojiCategory), findsOneWidget);
+      expect(find.byType(SubmitCategory), findsOneWidget);
+    });
+
+    testWidgets(
         'Show empty data in '
         '$TodoCategoryListView screen', (tester) async {
-      when(mockCategoryService.getCategories).thenAnswer(
-        (_) => Stream.value([]),
-      );
+      when(mockCategoryService.getCategories)
+          .thenAnswer((_) => Stream.value([]));
 
-      await tester.pumpWidget(ProviderScope(
-        overrides: [
-          categoryRepositoryProvider.overrideWithValue(categoryRepository),
-        ],
-        child: MaterialApp(
-          home: Scaffold(
-            body: Consumer(
-              builder: (_, ref, __) {
-                final categories = ref.watch(categoryDataProvider);
-                if (categories.data == null) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-
-                return categories.data!.value.isEmpty
-                    ? const Center(child: Text('Empty data'))
-                    : const Offstage();
-              },
-            ),
-          ),
-        ),
-      ));
+      await _pumpMainScreen(tester, const TodoCategoryListView());
 
       expect(find.byType(CircularProgressIndicator), findsOneWidget);
       await tester.pump(const Duration(seconds: 1));
 
       expect(find.byType(CircularProgressIndicator), findsNothing);
-      expect(find.text('Empty data'), findsOneWidget);
+      expect(find.text('Empty data, add a category'), findsOneWidget);
     });
 
     testWidgets('Show data $TodoCategoryListView in screen', (tester) async {
-      when(categoryRepository.getCategories).thenAnswer(
-        (_) => Stream.value([category]),
-      );
+      when(categoryRepository.getCategories)
+          .thenAnswer((_) => Stream.value([category]));
 
-      await tester.pumpWidget(ProviderScope(
-        overrides: [
-          categoryRepositoryProvider.overrideWithValue(categoryRepository),
-        ],
-        child: MaterialApp(home: _categoryListWithData()),
-      ));
+      await _pumpMainScreen(tester, const TodoCategoryListView());
 
       expect(find.byType(CircularProgressIndicator), findsOneWidget);
       await tester.pump(const Duration(seconds: 1));
@@ -127,19 +108,10 @@ void main() {
     testWidgets(
         'Make redirection when tap '
         'selected $Category to $TodoListView screen', (tester) async {
-      when(categoryRepository.getCategories).thenAnswer(
-        (_) => Stream.value([category]),
-      );
+      when(categoryRepository.getCategories)
+          .thenAnswer((_) => Stream.value([category]));
 
-      await tester.pumpWidget(ProviderScope(
-        overrides: [
-          categoryRepositoryProvider.overrideWithValue(categoryRepository),
-        ],
-        child: MaterialApp(
-          home: _categoryListWithData(),
-          navigatorObservers: [mockNavigator],
-        ),
-      ));
+      await _pumpMainScreen(tester, const TodoCategoryListView());
 
       expect(find.byType(CircularProgressIndicator), findsOneWidget);
       await tester.pump(const Duration(seconds: 1));
@@ -151,12 +123,13 @@ void main() {
         of: find.byType(GridView),
         matching: find.byType(CategoryItem),
       );
-
       expect(foundCatItem, findsOneWidget);
-      await tester.tap(foundCatItem);
 
+      await tester.tap(foundCatItem);
       await tester.pumpAndSettle();
-      verify(() => mockNavigator.didPush(any(), any())).called(1);
+
+      verify(() => mockNavigator.didPush(any(), any()));
+      expect(find.byType(TodoListView), findsOneWidget);
     });
   });
 }
