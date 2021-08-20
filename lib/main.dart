6e7ff7441d6f48cd28dab9b361d6972d086309e8
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-08-20 14:43:34 -0500

    Update main.dart adding route widget and become TodoListApp as stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:crud_todo_app/ui/todo_category_list_view.dart';
+import 'package:crud_todo_app/navigator/crud_todo_router_delegate.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -11,8 +11,15 @@ Future<void> main() async {
   runApp(const ProviderScope(child: TodoListApp()));
 }
 
-class TodoListApp extends StatelessWidget {
+class TodoListApp extends StatefulWidget {
   const TodoListApp({Key? key}) : super(key: key);
+
+  @override
+  _TodoListAppState createState() => _TodoListAppState();
+}
+
+class _TodoListAppState extends State<TodoListApp> {
+  final _todoRouterDelegate = CrudTodoRouterDelegate();
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +31,10 @@ class TodoListApp extends StatelessWidget {
           Theme.of(context).textTheme,
         ),
       ),
-      home: const TodoCategoryListView(),
+      home: Router<dynamic>(
+        routerDelegate: _todoRouterDelegate,
+        backButtonDispatcher: RootBackButtonDispatcher(),
+      ),
     );
   }
 }
