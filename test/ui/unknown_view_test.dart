f4f77ab9005f3d881fc5a3b8f333d95e89676d5f
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-09-12 14:25:22 -0500

    Create widget test in unknown_view_test.dart

---
+++ test/ui/unknown_view_test.dart
@@ -0,0 +1,33 @@
+import 'package:crud_todo_app/ui/unknown_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../test_utils/mocks.dart';
+
+void main() {
+  late MockNavigator mockNavigator;
+
+  setUpAll(() {
+    mockNavigator = MockNavigator();
+  });
+
+  Future<void> _pumpMainScreen(WidgetTester tester, Widget child) async {
+    await tester.pumpWidget(ProviderScope(
+      child: MaterialApp(
+        home: child,
+        navigatorObservers: [mockNavigator],
+      ),
+    ));
+  }
+
+  group('$UnknownView UI screen', () {
+    testWidgets('Show $UnknownView screen', (tester) async {
+      await _pumpMainScreen(tester, const UnknownView());
+
+      expect(find.text('Not found'), findsOneWidget);
+      expect(find.byType(Image), findsOneWidget);
+      expect(find.textContaining('Screen not found'), findsOneWidget);
+    });
+  });
+}
