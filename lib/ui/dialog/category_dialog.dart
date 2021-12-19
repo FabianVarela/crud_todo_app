3c99375a2fd1f8e0805c153437eabb18d1f5e373
Fabian Varela Bonett <jfabian.303@gmail.com>
2021-12-18 22:58:21 -0500

    Set category_dialog.dart as adaptive

--- lib/ui/dialog/category_dialog.dart
+++ lib/ui/dialog/category_dialog.dart
@@ -1,12 +1,14 @@
+import 'package:crud_todo_app/common/adaptive_contextual_layout.dart';
 import 'package:crud_todo_app/common/extension.dart';
 import 'package:crud_todo_app/provider_dependency.dart';
+import 'package:crud_todo_app/ui/widgets/close_icon_button.dart';
 import 'package:crud_todo_app/viewmodel/category/category_provider.dart';
 import 'package:crud_todo_app/viewmodel/category/category_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class CategoryFormDialog extends HookConsumerWidget {
+class CategoryFormDialog extends ConsumerWidget {
   const CategoryFormDialog({Key? key}) : super(key: key);
 
   @override
@@ -14,40 +16,52 @@ class CategoryFormDialog extends HookConsumerWidget {
     final categoryState = ref.watch(categoryViewModelProvider);
     final isValidForm = ref.watch(validationCategoryProvider);
 
+    final isDesktop = getFormFactor(context) == ScreenType.desktop;
+    final isTablet = getFormFactor(context) == ScreenType.tablet;
+
+    final desktopWidth = isDesktop || isTablet ? 600.0 : null;
+    final deviceWidth = !isPortrait(context) ? 400.0 : null;
+
     ref.listen(
       categoryViewModelProvider,
       (_, CategoryState state) => _onChangeState(context, state),
     );
 
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: <Widget>[
-        GestureDetector(
-          onTap: () => Navigator.of(context).pop(),
-          child: const Icon(Icons.close, color: Colors.white),
-        ).paddingSymmetric(v: 5),
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(12),
+    return SizedBox(
+      width: desktopWidth ?? deviceWidth,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          CloseIconButton(
+            onClick: () => Navigator.pop(context),
+          ).paddingSymmetric(v: 5),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+              children: <Widget>[
+                const Text(
+                  'Add category',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                ).paddingOnly(b: 15),
+                Column(
+                  children: <Widget>[
+                    const NameCategory().paddingOnly(b: 5),
+                    const EmojiCategory().paddingOnly(b: 25),
+                  ],
+                ),
+                if (!categoryState.isLoading)
+                  SubmitCategory(enabled: isValidForm)
+                else
+                  const CircularProgressIndicator()
+              ],
+            ).paddingSymmetric(h: 16, v: 10),
           ),
-          child: Column(
-            children: <Widget>[
-              const Text(
-                'Add category',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-              ).paddingOnly(b: 15),
-              const NameCategory().paddingOnly(b: 5),
-              const EmojiCategory().paddingOnly(b: 25),
-              if (!categoryState.isLoading)
-                SubmitCategory(enabled: isValidForm)
-              else
-                const CircularProgressIndicator()
-            ],
-          ).paddingSymmetric(h: 16, v: 10),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
@@ -99,7 +113,7 @@ class EmojiCategory extends HookConsumerWidget {
   }
 }
 
-class SubmitCategory extends HookConsumerWidget {
+class SubmitCategory extends ConsumerWidget {
   const SubmitCategory({Key? key, this.enabled = false}) : super(key: key);
 
   final bool enabled;
