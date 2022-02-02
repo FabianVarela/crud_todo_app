3fb16987940414d815e2cca7815204055660ee53
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-02-02 13:03:23 -0500

    Set window size if device is desktop and set onGenerateTitle

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
+import 'package:crud_todo_app/common/adaptive_contextual_layout.dart';
 import 'package:crud_todo_app/ui/category_list_view.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:window_size/window_size.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  if (getDevice() == DeviceSegment.desktop) {
+    setWindowMinSize(const Size(300, 500));
+    setWindowMaxSize(const Size(1500, 900));
+  }
 
   runApp(const ProviderScope(child: TodoListApp()));
 }
@@ -17,13 +24,19 @@ class TodoListApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'To-Do List App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
         textTheme: GoogleFonts.latoTextTheme(
           Theme.of(context).textTheme,
         ),
       ),
+      onGenerateTitle: (ctx) {
+        if (getDevice() == DeviceSegment.desktop) {
+          setWindowTitle('To-Do List App');
+        }
+        return 'To-Do List App';
+      },
       home: const CategoryListView(),
     );
   }
