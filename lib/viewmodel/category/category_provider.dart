98d928b6fdc5df7de88cd36e653e3d2fc651ce20
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-10-15 20:21:42 -0500

    Remove autoDispose by error with Flutter web and Riverpod

--- lib/viewmodel/category/category_provider.dart
+++ lib/viewmodel/category/category_provider.dart
@@ -4,22 +4,24 @@ import 'package:crud_todo_app/model/category_model.dart';
 import 'package:crud_todo_app/model/validation_text_model.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final categoryListPod = StreamProvider.autoDispose<List<Category>>(
+/*
+TODO: Restore autoDispose when error has been fixed
+  https://github.com/rrousselGit/riverpod/issues/1713
+  https://github.com/dart-lang/sdk/issues/50119
+*/
+
+final categoryListPod = StreamProvider<List<Category>>(
   (ref) => ref.watch(categoryRepositoryPod).getCategories(),
 );
 
-final categoryDetailPod = FutureProvider.autoDispose.family<Category, String>(
+final categoryDetailPod = FutureProvider.family<Category, String>(
   (ref, id) => ref.watch(categoryRepositoryPod).getCategoryById(id),
 );
 
-final nameCategoryPod = StateProvider.autoDispose(
-  (_) => const ValidationText(),
-);
-final emojiCategoryPod = StateProvider.autoDispose(
-  (_) => const ValidationText(),
-);
+final nameCategoryPod = StateProvider((_) => const ValidationText());
+final emojiCategoryPod = StateProvider((_) => const ValidationText());
 
-final validationCategoryPod = StateProvider.autoDispose((ref) {
+final validationCategoryPod = StateProvider((ref) {
   final name = ref.watch(nameCategoryPod).text;
   final emoji = ref.watch(emojiCategoryPod).text;
 
--- lib/viewmodel/todo/todo_provider.dart
+++ lib/viewmodel/todo/todo_provider.dart
@@ -4,25 +4,29 @@ import 'package:crud_todo_app/model/todo_model.dart';
 import 'package:crud_todo_app/model/validation_text_model.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final todoListPod = StreamProvider.autoDispose.family<List<Todo>, String>(
+/*
+TODO: Restore autoDispose when error has been fixed
+  https://github.com/rrousselGit/riverpod/issues/1713
+  https://github.com/dart-lang/sdk/issues/50119
+*/
+
+final todoListPod = StreamProvider.family<List<Todo>, String>(
   (ref, catId) => ref.watch(todoRepositoryPod).getTodosByCategory(catId),
 );
 
-final todoDetailPod = FutureProvider.autoDispose.family<Todo?, String>(
+final todoDetailPod = FutureProvider.family<Todo?, String>(
   (ref, param) {
     final params = param.split(',');
-    return params[0].isNotEmpty && params[1].isNotEmpty
+    return params[0].trim().isNotEmpty && params[1].trim().isNotEmpty
         ? ref.watch(todoRepositoryPod).getTodoById(params[0], params[1])
         : Future.value();
   },
 );
 
-final subjectTodoPod = StateProvider.autoDispose(
-  (_) => const ValidationText(),
-);
-final dateTodoPod = StateProvider.autoDispose((_) => DateTime.now());
+final subjectTodoPod = StateProvider((_) => const ValidationText());
+final dateTodoPod = StateProvider((_) => DateTime.now());
 
-final validationTodoProvider = StateProvider.autoDispose((ref) {
+final validationTodoProvider = StateProvider((ref) {
   final subject = ref.watch(subjectTodoPod).text;
   final date = ref.watch(dateTodoPod);
 
