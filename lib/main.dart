ebeb2739857cc97c220a9df6022e6b00c96f669b
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-10-15 23:05:01 -0500

    Add option to set firebase configuration depending on platform

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,13 @@ import 'dart:async';
 
 import 'package:crud_todo_app/app.dart';
 import 'package:crud_todo_app/bootstrap.dart';
+import 'package:crud_todo_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   await bootstrap(
     () => const TodoApp(title: 'To-Do List App'),
