b929fb7b3eab51fa0fa4c9d3f97ec222d16e308f
Fabian Varela Bonett <jfabian.303@gmail.com>
2022-10-31 20:00:38 -0500

    Fixing error riverpod setting autoDispose again

--- lib/viewmodel/category/category_provider.dart
+++ lib/viewmodel/category/category_provider.dart
@@ -4,24 +4,22 @@ import 'package:crud_todo_app/model/category_model.dart';
 import 'package:crud_todo_app/model/validation_text_model.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-/*
-TODO: Restore autoDispose when error has been fixed
-  https://github.com/rrousselGit/riverpod/issues/1713
-  https://github.com/dart-lang/sdk/issues/50119
-*/
-
-final categoryListPod = StreamProvider<List<Category>>(
+final categoryListPod = StreamProvider.autoDispose<List<Category>>(
   (ref) => ref.watch(categoryRepositoryPod).getCategories(),
 );
 
-final categoryDetailPod = FutureProvider.family<Category, String>(
+final categoryDetailPod = FutureProvider.family.autoDispose<Category, String>(
   (ref, id) => ref.watch(categoryRepositoryPod).getCategoryById(id),
 );
 
-final nameCategoryPod = StateProvider((_) => const ValidationText());
-final emojiCategoryPod = StateProvider((_) => const ValidationText());
+final nameCategoryPod = StateProvider.autoDispose(
+  (_) => const ValidationText(),
+);
+final emojiCategoryPod = StateProvider.autoDispose(
+  (_) => const ValidationText(),
+);
 
-final validationCategoryPod = StateProvider((ref) {
+final validationCategoryPod = StateProvider.autoDispose((ref) {
   final name = ref.watch(nameCategoryPod).text;
   final emoji = ref.watch(emojiCategoryPod).text;
 
--- lib/viewmodel/todo/todo_provider.dart
+++ lib/viewmodel/todo/todo_provider.dart
@@ -4,17 +4,11 @@ import 'package:crud_todo_app/model/todo_model.dart';
 import 'package:crud_todo_app/model/validation_text_model.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-/*
-TODO: Restore autoDispose when error has been fixed
-  https://github.com/rrousselGit/riverpod/issues/1713
-  https://github.com/dart-lang/sdk/issues/50119
-*/
-
-final todoListPod = StreamProvider.family<List<Todo>, String>(
+final todoListPod = StreamProvider.family.autoDispose<List<Todo>, String>(
   (ref, catId) => ref.watch(todoRepositoryPod).getTodosByCategory(catId),
 );
 
-final todoDetailPod = FutureProvider.family<Todo?, String>(
+final todoDetailPod = FutureProvider.family.autoDispose<Todo?, String>(
   (ref, param) {
     final params = param.split(',');
     return params[0].trim().isNotEmpty && params[1].trim().isNotEmpty
@@ -23,10 +17,10 @@ final todoDetailPod = FutureProvider.family<Todo?, String>(
   },
 );
 
-final subjectTodoPod = StateProvider((_) => const ValidationText());
-final dateTodoPod = StateProvider((_) => DateTime.now());
+final subjectTodoPod = StateProvider.autoDispose((_) => const ValidationText());
+final dateTodoPod = StateProvider.autoDispose((_) => DateTime.now());
 
-final validationTodoProvider = StateProvider((ref) {
+final validationTodoProvider = StateProvider.autoDispose((ref) {
   final subject = ref.watch(subjectTodoPod).text;
   final date = ref.watch(dateTodoPod);
 
