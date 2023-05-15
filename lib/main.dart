cd891c9b9b6cb7f0997f2aad437406c6acb75dab
Fabian Varela Bonett <jfabian.303@gmail.com>
2023-05-14 20:47:25 -0500

    Modify main files to avoid error in Flutter 3.10

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
-import 'dart:async';
-
 import 'package:crud_todo_app/app.dart';
 import 'package:crud_todo_app/bootstrap.dart';
 import 'package:crud_todo_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+void main() {
+  bootstrap(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
 
-  await bootstrap(
-    () => const TodoApp(title: 'To-Do List App'),
-  );
+    return const TodoApp(title: 'To-Do List App');
+  });
 }
