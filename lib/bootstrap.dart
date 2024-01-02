f753c5f9f53884008882a9a027f6c97d9385f60f
FabianVarela <jfabian.303@gmail.com>
2024-01-02 16:49:35 -0500

    Add FirebaseOption parameter and move Bindings before runApp

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,26 +2,31 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:crud_todo_app/common/adaptive_contextual_layout.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:window_size/window_size.dart';
 
-Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
-  if (getDevice() == DeviceSegment.desktop) {
-    setWindowMinSize(const Size(300, 500));
-    setWindowMaxSize(const Size(1500, 900));
-  }
-
+Future<void> bootstrap(
+  FutureOr<Widget> Function() builder,
+  FirebaseOptions options,
+) async {
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
 
   await runZonedGuarded(
-    () async => runApp(
-      ProviderScope(
-        child: await builder(),
-      ),
-    ),
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp(options: options);
+
+      if (getDevice() == DeviceSegment.desktop) {
+        setWindowMinSize(const Size(300, 500));
+        setWindowMaxSize(const Size(1500, 900));
+      }
+
+      runApp(ProviderScope(child: await builder()));
+    },
     (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
   );
 }
--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,16 +1,10 @@
 import 'package:crud_todo_app/app.dart';
 import 'package:crud_todo_app/bootstrap.dart';
 import 'package:crud_todo_app/firebase_options_dev.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 
 void main() {
-  bootstrap(() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-
-    return const TodoApp(title: 'To-Do List App - Dev');
-  });
+  bootstrap(
+    () async => const TodoApp(title: 'To-Do List App - Dev'),
+    DefaultFirebaseOptions.currentPlatform,
+  );
 }
