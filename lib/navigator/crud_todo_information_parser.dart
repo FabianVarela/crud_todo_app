112cd18fd3b70da91f62271f37a70fc6ca9a82fe
FabianVarela <jfabian.303@gmail.com>
2024-01-01 23:28:49 -0500

    Fix linter issues and deprecated values

--- lib/navigator/crud_todo_information_parser.dart
+++ lib/navigator/crud_todo_information_parser.dart
@@ -16,7 +16,7 @@ class CrudTodoInformationParser extends RouteInformationParser<CrudTodoConfig> {
   Future<CrudTodoConfig> parseRouteInformation(
     RouteInformation routeInformation,
   ) async {
-    final uri = Uri.parse(routeInformation.location ?? '');
+    final uri = routeInformation.uri;
     final newPaths = uri.pathSegments.map((seg) => seg.toLowerCase()).toList();
 
     switch (newPaths.length) {
@@ -26,13 +26,11 @@ class CrudTodoInformationParser extends RouteInformationParser<CrudTodoConfig> {
         if (newPaths[0] == TodoPath.category.name) {
           return const CrudTodoConfigCategoryList();
         }
-        break;
       case 2:
         final segments = (newPaths[0], newPaths[1]);
         if (segments.$1 == TodoPath.category.name && segments.$2.isNotEmpty) {
           return CrudTodoConfigTodoList(segments.$2);
         }
-        break;
       case 3:
         final segments = (newPaths[0], newPaths[1], newPaths[2]);
         if (segments.$1 == TodoPath.category.name) {
@@ -40,7 +38,6 @@ class CrudTodoInformationParser extends RouteInformationParser<CrudTodoConfig> {
             return CrudTodoConfigAddTodo(segments.$2);
           }
         }
-        break;
       case 4:
         final segments = (newPaths[0], newPaths[1], newPaths[2], newPaths[3]);
         if (segments.$1 == TodoPath.category.name && segments.$2.isNotEmpty) {
@@ -48,7 +45,6 @@ class CrudTodoInformationParser extends RouteInformationParser<CrudTodoConfig> {
             return CrudTodoConfigUpdateTodo(segments.$2, segments.$4);
           }
         }
-        break;
     }
 
     return const CrudTodoConfig.unknown();
@@ -58,18 +54,22 @@ class CrudTodoInformationParser extends RouteInformationParser<CrudTodoConfig> {
   RouteInformation? restoreRouteInformation(CrudTodoConfig configuration) {
     return configuration.when(
       categoryList: () => RouteInformation(
-        location: '/${TodoPath.category.name}',
+        uri: Uri.parse('/${TodoPath.category.name}'),
       ),
       todoList: (categoryId) => RouteInformation(
-        location: '/${TodoPath.category.name}/$categoryId',
+        uri: Uri.parse('/${TodoPath.category.name}/$categoryId'),
       ),
       addTodo: (id) => RouteInformation(
-        location: '/${TodoPath.category.name}/$id/${TodoPath.todo.name}/',
+        uri: Uri.parse('/${TodoPath.category.name}/$id/${TodoPath.todo.name}/'),
       ),
       updateTodo: (id, tId) => RouteInformation(
-        location: '/${TodoPath.category.name}/$id/${TodoPath.todo.name}/$tId',
+        uri: Uri.parse(
+          '/${TodoPath.category.name}/$id/${TodoPath.todo.name}/$tId',
+        ),
       ),
-      unknown: () => RouteInformation(location: '/${TodoPath.unknown.name}'),
+      unknown: () => RouteInformation(
+        uri: Uri.parse('/${TodoPath.unknown.name}'),
+      ),
     );
   }
 }
