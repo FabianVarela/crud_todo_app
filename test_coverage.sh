e4f36faedc30ea8883c08fc18d3dbce6385cde94
FabianVarela <jfabian.303@gmail.com>
2024-01-13 14:40:49 -0500

    Modify deprecated commands

--- test_coverage.sh
+++ test_coverage.sh
@@ -1,5 +1,5 @@
 flutter test --coverage
-flutter pub run remove_from_coverage -f coverage/lcov.info -r '\.g\.dart$'
-flutter pub run remove_from_coverage -f coverage/lcov.info -r '\.freezed\.dart$'
+dart run remove_from_coverage -f coverage/lcov.info -r '\.g\.dart$'
+dart run remove_from_coverage -f coverage/lcov.info -r '\.freezed\.dart$'
 genhtml coverage/lcov.info -o coverage/html -q
 open coverage/html/index.html
