22de01cefee2b9a5ab01d0cf780760466a962600
FabianVarela <jfabian.303@gmail.com>
2024-03-15 15:02:48 -0500

    Improve test for unknown page

--- test/ui/unknown_view_test.dart
+++ test/ui/unknown_view_test.dart
@@ -1,28 +1,63 @@
+import 'package:crud_todo_app/dependency/dependency.dart';
+import 'package:crud_todo_app/navigator/crud_todo_information_parser.dart';
+import 'package:crud_todo_app/navigator/crud_todo_router_delegate.dart';
+import 'package:crud_todo_app/repository/category_repository.dart';
 import 'package:crud_todo_app/ui/unknown_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mocktail/mocktail.dart';
 
 import '../test_utils/mocks.dart';
 
 void main() {
-  late MockNavigator mockNavigator;
+  late final MockFirestore mockFirestoreInstance;
+
+  late final MockCategoryService mockCategoryService;
+  late final ICategoryRepository categoryRepository;
+
+  late final CrudTodoRouterDelegate todoRouterDelegate;
+  late final CrudTodoInformationParser todoInfoParser;
 
   setUpAll(() {
-    mockNavigator = MockNavigator();
+    mockFirestoreInstance = MockFirestore();
+
+    mockCategoryService = MockCategoryService();
+    categoryRepository = CategoryRepository(mockCategoryService);
+    registerFallbackValue(MyCategoryFake());
+
+    // Using simple Riverpod to test dependencies
+    final container = ProviderContainer();
+
+    todoRouterDelegate = container.read(crudTodoRouterDelegateProvider);
+    todoInfoParser = container.read(crudTodoInformationParserProvider);
   });
 
-  Future<void> pumpMainScreen(WidgetTester tester, Widget child) async {
+  Future<void> pumpMainScreen(WidgetTester tester) async {
     await tester.pumpWidget(
       ProviderScope(
-        child: MaterialApp(home: child, navigatorObservers: [mockNavigator]),
+        overrides: [
+          firebasePod.overrideWithValue(mockFirestoreInstance),
+          categoryServicePod.overrideWithValue(mockCategoryService),
+          categoryRepositoryPod.overrideWithValue(categoryRepository),
+        ],
+        child: MaterialApp.router(
+          routerDelegate: todoRouterDelegate,
+          routeInformationParser: todoInfoParser,
+          backButtonDispatcher: RootBackButtonDispatcher(),
+        ),
       ),
     );
   }
 
   group('$UnknownView UI screen', () {
     testWidgets('Show $UnknownView screen', (tester) async {
-      await pumpMainScreen(tester, const UnknownView());
+      await pumpMainScreen(tester);
+
+      todoRouterDelegate.is404 = true;
+      await tester.pumpAndSettle();
+
+      expect(find.byType(UnknownView), findsOneWidget);
 
       expect(find.text('Not found'), findsOneWidget);
       expect(find.byType(Image), findsOneWidget);
