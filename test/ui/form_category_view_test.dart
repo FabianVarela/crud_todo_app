2dfbe23bb51960e548920eefe204941a8ea3cbf9
FabianVarela <jfabian.303@gmail.com>
2024-08-31 14:24:56 -0500

    Modify test file name

--- test/ui/form_category_view_test.dart
+++ test/ui/form_category_view_test.dart
@@ -10,7 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mocktail/mocktail.dart';
 
-import '../../test_utils/mocks.dart';
+import '../test_utils/mocks.dart';
 
 void main() {
   group('$FormCategoryView UI dialog', () {
