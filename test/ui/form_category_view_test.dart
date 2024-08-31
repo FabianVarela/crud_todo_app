10fc7d6244e6ebe012d7aa595e74974acf74321f
FabianVarela <jfabian.303@gmail.com>
2024-08-31 15:03:13 -0500

    Improve form category view test file

--- test/ui/form_category_view_test.dart
+++ test/ui/form_category_view_test.dart
@@ -1,5 +1,7 @@
 import 'package:crud_todo_app/dependency/dependency.dart';
 import 'package:crud_todo_app/model/category_model.dart';
+import 'package:crud_todo_app/navigator/crud_todo_information_parser.dart';
+import 'package:crud_todo_app/navigator/crud_todo_router_delegate.dart';
 import 'package:crud_todo_app/repository/category_repository.dart';
 import 'package:crud_todo_app/ui/form_category_view.dart';
 import 'package:crud_todo_app/ui/widgets/custom_mouse_region.dart';
@@ -19,15 +21,26 @@ void main() {
     late final MockCategoryService mockCategoryService;
     late final ICategoryRepository categoryRepository;
 
+    late final CrudTodoRouterDelegate todoRouterDelegate;
+    late final CrudTodoInformationParser todoInfoParser;
+
+    late CategoryViewModel categoryViewModel;
+
     setUpAll(() {
       mockFirestoreInstance = MockFirestore();
 
       mockCategoryService = MockCategoryService();
       categoryRepository = CategoryRepository(mockCategoryService);
       registerFallbackValue(MyCategoryFake());
+
+      // Using simple Riverpod to test dependencies
+      final container = ProviderContainer();
+
+      todoRouterDelegate = container.read(crudTodoRouterDelegateProvider);
+      todoInfoParser = container.read(crudTodoInformationParserProvider);
     });
 
-    Future<void> pumpDialog(WidgetTester tester, Widget child) async {
+    Future<void> pumpMainScreen(WidgetTester tester) async {
       await tester.pumpWidget(
         ProviderScope(
           overrides: [
@@ -35,15 +48,40 @@ void main() {
             categoryServiceProvider.overrideWithValue(mockCategoryService),
             categoryRepositoryProvider.overrideWithValue(categoryRepository),
           ],
-          child: MaterialApp(home: child),
+          child: MaterialApp.router(
+            routerDelegate: todoRouterDelegate,
+            routeInformationParser: todoInfoParser,
+            backButtonDispatcher: RootBackButtonDispatcher(),
+            builder: (_, child) => Consumer(
+              builder: (_, ref, __) {
+                categoryViewModel = ref.read(categoryViewModelPod.notifier);
+                return child!;
+              },
+            ),
+          ),
         ),
       );
+    }
+
+    Future<void> initScreensAndRedirect(WidgetTester tester) async {
+      await pumpMainScreen(tester);
+      await tester.pump(const Duration(seconds: 1));
+
+      final button = find.byType(FloatingActionButton);
+
+      await tester.ensureVisible(button);
+      await tester.pumpAndSettle();
+
+      await tester.tap(button);
+      await tester.pumpAndSettle();
+
+      expect(find.byType(FormCategoryView), findsOneWidget);
     }
 
     testWidgets(
       'Click on close button and verify if close it ',
       (tester) async {
-        await pumpDialog(tester, const Scaffold(body: FormCategoryView()));
+        await initScreensAndRedirect(tester);
 
         expect(find.byType(CustomMouseRegion), findsOneWidget);
 
@@ -62,7 +100,7 @@ void main() {
     testWidgets(
       'Show $SubmitCategory disabled when text fields forms are empty',
       (tester) async {
-        await pumpDialog(tester, const Scaffold(body: FormCategoryView()));
+        await initScreensAndRedirect(tester);
 
         final foundButton = find.byType(SubmitCategory);
 
@@ -78,7 +116,7 @@ void main() {
     testWidgets(
       'Show $SubmitCategory enabled when text field forms are not empty',
       (tester) async {
-        await pumpDialog(tester, const Scaffold(body: FormCategoryView()));
+        await initScreensAndRedirect(tester);
 
         final foundSubmitButton = find.byType(SubmitCategory);
 
@@ -98,21 +136,11 @@ void main() {
     testWidgets(
       'Add $Category data from $FormCategoryView form fields',
       (tester) async {
-        late final CategoryViewModel viewModel;
-
         when(() => mockCategoryService.saveCategory(any())).thenAnswer(
           (_) => Future<void>.delayed(const Duration(seconds: 1)),
         );
 
-        await pumpDialog(
-          tester,
-          Consumer(
-            builder: (_, ref, child) {
-              viewModel = ref.read(categoryViewModelPod.notifier);
-              return const Scaffold(body: FormCategoryView());
-            },
-          ),
-        );
+        await initScreensAndRedirect(tester);
 
         await tester.enterText(find.byType(NameCategory), 'Test Category');
         await tester.enterText(find.byType(EmojiCategory), '😀');
@@ -122,13 +150,13 @@ void main() {
 
         verify(() => mockCategoryService.saveCategory(any())).called(1);
 
-        expect(viewModel.state.isLoading, true);
+        expect(categoryViewModel.state.isLoading, true);
         await tester.pump(const Duration(milliseconds: 500));
 
         expect(find.byType(CircularProgressIndicator), findsOneWidget);
         await tester.pumpAndSettle();
 
-        expect(viewModel.state.isSuccess, true);
+        expect(categoryViewModel.state.isSuccess, true);
         expect(find.byType(CircularProgressIndicator), findsNothing);
       },
       variant: TargetPlatformVariant.all(),
@@ -137,21 +165,11 @@ void main() {
     testWidgets(
       'When try to add a $Category data set an $Exception',
       (tester) async {
-        late final CategoryViewModel viewModel;
-
         when(() => mockCategoryService.saveCategory(any())).thenThrow(
           Exception('Error'),
         );
 
-        await pumpDialog(
-          tester,
-          Consumer(
-            builder: (_, ref, child) {
-              viewModel = ref.read(categoryViewModelPod.notifier);
-              return const Scaffold(body: FormCategoryView());
-            },
-          ),
-        );
+        await initScreensAndRedirect(tester);
 
         await tester.enterText(find.byType(NameCategory), 'Test Category');
         await tester.enterText(find.byType(EmojiCategory), '😀');
@@ -160,7 +178,7 @@ void main() {
         await tester.tap(find.byType(SubmitCategory));
 
         verify(() => mockCategoryService.saveCategory(any())).called(1);
-        expect(viewModel.state.isError, true);
+        expect(categoryViewModel.state.isError, true);
       },
       variant: TargetPlatformVariant.all(),
     );
