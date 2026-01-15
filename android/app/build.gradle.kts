7fee492c2801f48ab540c25fa5cd24f28f604d8c
FabianVarela <jfabian.303@gmail.com>
2026-01-15 13:56:51 -0500

    Update android files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,8 +8,8 @@ plugins {
 
 android {
     namespace = "com.developer.fabian.crud_todo_app"
-    compileSdk = 35
-    ndkVersion = "27.0.12077973"
+    compileSdk = 36
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
